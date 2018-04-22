4e22f2a2554bcb12be954e26720dd5e7e3fbe8aa
Jakub <sirthaven@gmail.com>
2018-04-22 22:23:43 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import './App.css';
+
+import logo from './logo.svg';
+
+class App extends React.Component {
+
+
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+         First, open a CSV File 
+        </p>
+        <input name="myFile" type="file" />
+        
+      </div>
+    );
+  }
+}
+
+export default App;
