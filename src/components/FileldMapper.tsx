9253c6d3dee09bae8be2545a8ac916da238ee926
Jakub <sirthaven@gmail.com>
2018-05-11 21:25:57 +0200

    fighting with select controll

--- src/components/FileldMapper.tsx
+++ src/components/FileldMapper.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
 import { ITransformator } from "../logic/Transformations/transformator";
 import { VoidTransformator } from "../logic/Transformations/VoidTransformation";
+import Select from 'react-select';
+// import 'react-select/dist/react-select.es';
 
 interface IFiledMapperProp
 {
@@ -41,16 +43,25 @@ export class FieldMapper extends React.Component<IFiledMapperProp,IFilledMapperS
 
     private buildDropdown()
     {
-        return <div className="dropdown" onChange={this.onChange}>
-        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Add new transformation
-        </button>
-        <div className="dropdown-menu">
-        {this.props.transformatorsTypes.map(tt=>
-          <a className="dropdown-item" href="#" onClick={this.onChange}  key={tt} >{tt}</a>        
-        )}
-        </div>
-      </div>
+        return   <Select
+        name="form-field-name"
+        value="ohoho"
+        onChange={this.onChange}
+        options={[
+          { value: 'one', label: 'One' },
+          { value: 'two', label: 'Two' },
+        ]}
+      />
+    //     return <div className="dropdown" onChange={this.onChange}>
+    //     <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    //       Add new transformation
+    //     </button>
+    //     <div className="dropdown-menu">
+    //     {this.props.transformatorsTypes.map(tt=>
+    //       <a className="dropdown-item" href="#" onClick={this.onChange}  key={tt} >{tt}</a>        
+    //     )}
+    //     </div>
+    //   </div>
     }
 
     private onChange = (e:any) => {alert("changed")}
